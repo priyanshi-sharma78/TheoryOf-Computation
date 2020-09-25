d00b1e3bf2d0e618b2a31980b53a60607f436afe
PriyanshiSharma <62379772+priyanshi-sharma78@users.noreply.github.com>
2020-09-25 17:54:56 +0530

    ending with 101

---
+++ string always ending with 101.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool is valid(string S)
+{ int n=s.size();
+if (s[n-1]=='1' && s[n-2]=='0' && s[n-3]=='1')
+return false;
+}
+int main()
+{ string name;
+cin>>name;
+if(isvalid(name))
+cout<<"string accepted";
+else
+cout<<"string not accepted";
+}
